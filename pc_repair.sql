a7b7b8c4d9777a1131ea70ff421702407c6f28e2
Girukami <jgm.eclipse@gmail.com>
2024-04-18 15:04:11 +0800

    update sql line 74

--- pc_repair.sql
+++ pc_repair.sql
@@ -71,6 +71,7 @@ CREATE TABLE `appointment_service` (
   `service_id` int(11) NOT NULL,
   `address` varchar(255) NOT NULL,
   `contact_num` varchar(255) NOT NULL
+  `specreq` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
