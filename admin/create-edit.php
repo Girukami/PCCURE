c52481853a9deed4801e3af4d8e5322a30d09ffc
Girukami <jgm.eclipse@gmail.com>
2024-04-26 23:03:35 +0800

    try

--- admin/create-edit.php
+++ admin/create-edit.php
@@ -175,10 +175,10 @@
                 </div>
 
                 <label for="dropdown">Select service Provider:</label>
-                <select name="dropdown" id="dropdown" value="<?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">
-                    <option value="option1">R-Shariff A. Asula</option>
-                    <option value="option2">John Gil M. Eclipse</option>
-                    <option value="option3">Benkhair A. Najir</option>
+                <select name="dropdown" id="dropdown">
+                    <option value="option1 <?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">R-Shariff A. Asula</option>
+                    <option value="option2 <?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">John Gil M. Eclipse</option>
+                    <option value="option3 <?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">Benkhair A. Najir</option>
                 </select>
                 
 
