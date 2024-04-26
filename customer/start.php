b089fe8f1614660ee862df01f1b1b9afe0205627
Girukami <jgm.eclipse@gmail.com>
2024-04-26 19:34:27 +0800

    test

--- customer/start.php
+++ customer/start.php
@@ -214,23 +214,23 @@ body {
       <div class="w3-twothird">
         <h1>WHAT WE DO</h1>
         <h5 class="w3-padding-32 justified  "></h5>
-        <div class="slideshow-container">
+        <div class="slideshow-container w3-center">
 
           <div class="mySlides fade">
             <div class="numbertext">1 / 3</div>
-            <img src="img_nature_wide.jpg" style="width:100%">
+            <img src="1.jpg" style="width:100%">
             <div class="text">Caption Text</div>
           </div>
 
           <div class="mySlides fade">
             <div class="numbertext">2 / 3</div>
-            <img src="img_snow_wide.jpg" style="width:100%">
+            <img src="2.jpg" style="width:100%">
             <div class="text">Caption Two</div>
           </div>
 
           <div class="mySlides fade">
             <div class="numbertext">3 / 3</div>
-            <img src="img_mountains_wide.jpg" style="width:100%">
+            <img src="3.jpg" style="width:100%">
             <div class="text">Caption Three</div>
           </div>
 
