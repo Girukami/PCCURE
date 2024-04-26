3d36b56fd8922c23833d426da1ce43fd13e876ea
Girukami <jgm.eclipse@gmail.com>
2024-04-26 23:07:12 +0800

    upd

--- admin/create-edit.php
+++ admin/create-edit.php
@@ -176,9 +176,9 @@
 
                 <label for="dropdown">Select service Provider:</label>
                 <select name="dropdown" id="dropdown">
-                    <option value="option1 <?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">R-Shariff A. Asula</option>
-                    <option value="option2 <?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">John Gil M. Eclipse</option>
-                    <option value="option3 <?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">Benkhair A. Najir</option>
+                    <option value="<?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">R-Shariff A. Asula</option>
+                    <option value="<?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">John Gil M. Eclipse</option>
+                    <option value="<?php if(isset($service_id)){ echo $service_info['worker']; } if(isset($_POST['worker'])){ echo $_POST['worker']; } ?>">Benkhair A. Najir</option>
                 </select>
                 
 
