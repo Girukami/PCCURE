f45cc3c602ae4ce1c41fc0369f04d7f8c9ce2b35
Girukami <jgm.eclipse@gmail.com>
2024-04-28 19:16:19 +0800

    try

--- customer/signup.php
+++ customer/signup.php
@@ -52,78 +52,50 @@
         </div>
 
         <div class="inputs">
-            <div>
-                <label for="fname">First Name</label>
-                <input
-                    type="text"
-                    id="fname"
-                    name="fname"
-                    placeholder="Enter first name" />
-            </div>
-
-            <div>
-                <label for="mname">Middle Name</label>
-                <input
-                    type="text"
-                    id="mname"
-                    name="mname"
-                    placeholder="Enter middle name" />
-            </div>
-
-            <div>
-                <label for="lname">Last Name</label>
-                <input
-                    type="text"
-                    id="lname"
-                    name="lname"
-                    placeholder="Enter last name" />
-            </div>
-
-            <div>
-                <label for="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Enter email" />
-            </div>
-
-            <div>
-                <label for="cnum">Contact Number</label>
-                <input
-                    type="text"
-                    id="cnum"
-                    name="cnum"
-                    placeholder="Enter contact number" />
-            </div>
-
-            <div>
-                <label for="address">Address</label>
-                <input
-                    type="text"
-                    id="address"
-                    name="address"
-                    placeholder="Enter complete address" />
-            </div>
-
-            <div>
-                <label for="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Enter password" />
-            </div>
-
-            <div>
-                <label for="password2">Confirm Password</label>
-                <input
-                    type="password"
-                    id="password2"
-                    name="password2"
-                    placeholder="Re-enter password" />
-            </div>
+        <div>
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="fname" placeholder="Enter first name" value="<?php echo isset($_POST['fname']) ? htmlspecialchars($_POST['fname']) : ''; ?>" />
         </div>
+
+        <div>
+            <label for="mname">Middle Name</label>
+            <input type="text" id="mname" name="mname" placeholder="Enter middle name" value="<?php echo isset($_POST['mname']) ? htmlspecialchars($_POST['mname']) : ''; ?>" />
+        </div>
+
+        <div>
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname" name="lname" placeholder="Enter last name" value="<?php echo isset($_POST['lname']) ? htmlspecialchars($_POST['lname']) : ''; ?>" />
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>" />
+        </div>
+
+        <div>
+            <label for="cnum">Contact Number</label>
+            <input type="text" id="cnum" name="cnum" placeholder="Enter contact number" value="<?php echo isset($_POST['cnum']) ? htmlspecialchars($_POST['cnum']) : ''; ?>" />
+        </div>
+
+        <div>
+            <label for="address">Address</label>
+            <input type="text" id="address" name="address" placeholder="Enter complete address" value="<?php echo isset($_POST['address']) ? htmlspecialchars($_POST['address']) : ''; ?>" />
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter password" />
+        </div>
+
+        <div>
+            <label for="password2">Confirm Password</label>
+            <input type="password" id="password2" name="password2" placeholder="Re-enter password" />
+        </div>
+
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </div>
 
         <div class="actions">
             <div class="buttons">
